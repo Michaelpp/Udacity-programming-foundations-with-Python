c839b8f815a8826b8e80c6c96768903dac0d2562
Michaelpp <cloudburn.y@gmail.com>
2017-04-07 18:47:33 +0800

    Create print_abacus.py

---
+++ print_abacus.py
@@ -0,0 +1,104 @@
+#########################################################################
+#                 10-row School abacus
+#                         by
+#                      Michael H
+#########################################################################
+#       Description partially extracted from from wikipedia 
+#
+#  Around the world, abaci have been used in pre-schools and elementary
+#
+# In Western countries, a bead frame similar to the Russian abacus but
+# with straight wires and a vertical frame has been common (see image).
+# Helps schools as an aid in teaching the numeral system and arithmetic
+#
+#         |00000*****   |     row factor 1000000000
+#         |00000*****   |     row factor 100000000
+#         |00000*****   |     row factor 10000000 
+#         |00000*****   |     row factor 1000000
+#         |00000*****   |     row factor 100000
+#         |00000*****   |     row factor 10000
+#         |00000*****   |     row factor 1000
+#         |00000****   *|     row factor 100     * 1
+#         |00000***   **|     row factor 10      * 2
+#         |00000**   ***|     row factor 1       * 3
+#                                        -----------    
+#                             Sum                123 
+#
+# Each row represents a different row factor, starting with x1 at the
+# bottom, ascending up to x1000000000 at the top row.     
+######################################################################
+
+# TASK:
+# Define a procedure print_abacus(integer) that takes a positive integer
+# and prints a visual representation (image) of an abacus setup for a 
+# given positive integer value.
+# 
+# Ranking
+# 1 STAR: solved the problem!
+# 2 STARS: 6 < lines <= 9
+# 3 STARS: 3 < lines <= 6
+# 4 STARS: 0 < lines <= 3
+
+# HERE ARR THE CODES
+def print_abacus(value):
+    len_num = len(str(value))
+    if len_num <= 10:
+        for i in range(10-len_num):
+            print "|"+"0"*5+"*"*5+"   "+"|"
+        for i in str(value):
+            if int(i)>=5:
+                print "|"+"0"*(10-int(i))+"   "+"0"*(int(i)-5)+"*"*5+"|"
+            else:
+                print "|"+"0"*5+"*"*(5-int(i))+"   "+"*"*int(i)+"|"
+         
+#ANOTHER WAY TO SOLVE THIS PROBLEM
+print "=================="
+def print_abacus(value):
+    ndigits = len(str(value))
+    blank_abacus = '|00000*****|'
+    my_abacus =""
+    for n in range(10,0,-1):
+        if n <= ndigits:
+            digit_value = int(str(value)[ndigits-n])
+            my_abacus = my_abacus + blank_abacus[0:10-digit_value+1] + '   ' + blank_abacus[10-digit_value+1:] +"\n"
+        else:
+            my_abacus = my_abacus + blank_abacus[:-1] + '   ' + '|' + "\n"
+
+
+###  TEST CASES
+print "Abacus showing 0:"
+print_abacus(0)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+print "Abacus showing 12345678:"
+print_abacus(12345678)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000****   *|
+#>>>|00000***   **|
+#>>>|00000**   ***|
+#>>>|00000*   ****|
+#>>>|00000   *****|
+#>>>|0000   0*****|
+#>>>|000   00*****|
+#>>>|00   000*****|
+print "Abacus showing 1337:"
+print_abacus(1337)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000****   *|
+#>>>|00000**   ***|
+#>>>|00000**   ***|
+#>>>|000   00*****|
